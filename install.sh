cbe10cb088ebc629f7bdc6a4c9e799e49a783f84
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-16 13:59:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ cd build
 wget -O .replit https://raw.githubusercontent.com/ok8023/PanIndex_To_Replit/main/.replit
 wget -O replit.nix https://raw.githubusercontent.com/ok8023/PanIndex_To_Replit/main/files/replit.nix
 cd ..
-
 wget -O main.sh https://raw.githubusercontent.com/ok8023/PanIndex_To_Replit/main/files/main.sh
 wget -O install.sh https://raw.githubusercontent.com/ok8023/PanIndex_To_Replit/main/files/run.sh
 wget -O README.md https://raw.githubusercontent.com/ok8023/PanIndex_To_Replit/main/README.md
 cp -r build/.replit . && cp -r build/replit.nix .
+rm -rf build/
+sh install.sh
 echo "恭喜项目搭建完成"
 echo "点击Run立即运行项目"
-echo "查询用户名及密码运行下面命令"
-echo "./sx admin"
-rm -rf build/
+echo "默认密码为:PanIndex"
